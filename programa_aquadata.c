8ff059757272ca3d60b41f3f1239cfff984628a2
Macarenabfernandez <macarena.bfernandez@alumnos.upm.es>
2023-05-08 15:59:14 +0200

    mas cambios

--- programa_aquadata.c
+++ programa_aquadata.c
@@ -8,7 +8,11 @@ struct Tfuente{
 	int conductividad;
 	int turbidez;
 	int coliformes;
-	int puntos;
+};
+
+struct TPuntos{
+    char fuente[10];
+    int puntos;
 };
 
 void buscarph(struct Tfuente barrio[], float);
@@ -24,6 +28,8 @@ float mediascoliformes(struct Tfuente barrio[],int);
 
 int main () {
 	struct Tfuente barrio[500];
+	struct TPuntos ranking[500];
+	struct TPuntos mejorfuente[500];
 	int nfuentes;
 	char parametro;
 	char max[25];
@@ -35,7 +41,7 @@ int main () {
 	FILE *fentrada;
 	FILE *fsalida;
 	int puntos=0;
-	fentrada = fopen("202301_Lavapies.txt", "r"); 
+	fentrada = fopen("202301_Lavapies.txt", "r");
 	if (fentrada == NULL) {
 		printf("Error, no puede abrir el fichero.\n");
 		return 0;
@@ -49,7 +55,7 @@ int main () {
 
 	printf("Introduce la opcion que desee\n");
 	printf("1: Buscar Datos de la fuente\n");
-	printf("2: Anadir datos de parametro u otra fuente\n");
+	printf("2: Anadir otra fuente\n");
 	printf("3: Información\n");
 	printf("4: Salir del programa\n");
 	scanf("%d", &opcion);
@@ -63,7 +69,7 @@ int main () {
 			fflush(stdin);
 			scanf ("%c", &opciondato);
 			switch(opciondato){
-			
+
 	//	}while ( opciondato != 'B' && opciondato != 'E');
 
 
@@ -82,7 +88,7 @@ int main () {
             			printf("Introduce el pH del agua buscado que se encuentra entre 6 y 8\n");
             			scanf("%f", &ph1);
                         buscarph(barrio, ph1);}
-                        
+
 	 			else if(parametro=='C'){
 	 				printf("Introduce la conductIvidad del agua buscada que se encuentra entre 100 y 300\n");
             			scanf("%d", &conductividad);
@@ -100,48 +106,47 @@ int main () {
 break;
 }while(parametro!='P' || parametro!='C' || parametro!='T' ||parametro!='F');
 
-          break; 
+          break;
             case 'e':
             case 'E':
             	printf("En las estadisticas de las fuentes apreciaremos las medias de cada parámetro de la fuente y un ranking de Las 3 mejores y Las 3 peores fuentes");
             	for(i=1;i<=25;i++){
                     if(barrio[i].pH >= 6.8 && barrio[i].pH <= 7.2){
-                        barrio[i].puntos = 3;
+                        ranking[i].puntos = 3;
                     } else if(barrio[i].pH > 7.2 && barrio[i].pH <= 8){
-                        barrio[i].puntos = 2;
+                        ranking[i].puntos = 2;
                     }else if(barrio[i].pH >=6 && barrio[i].pH < 6.8){
-                        barrio[i].puntos = 1;
+                        ranking[i].puntos = 1;
                     }else {
-                        barrio[i].puntos = 0;}
+                        ranking[i].puntos = 0;}
                     if(barrio[i].conductividad >= 190){
-                        barrio[i].puntos += 3;
+                        ranking[i].puntos += 3;
                     } else if (barrio[i].conductividad >= 145){
-                        barrio[i].puntos += 2;
+                        ranking[i].puntos += 2;
                     } else if (barrio[i].conductividad >= 100){
-                        barrio[i].puntos += 1;
+                        ranking[i].puntos += 1;
                     }else {
-                        barrio[i].puntos += 0;
+                        ranking[i].puntos += 0;
                     }
                     if(barrio[i].turbidez == 0){
-                        barrio[i].puntos += 3;
+                        ranking[i].puntos += 3;
                     }else if (barrio[i].turbidez >= 1 && barrio[i].turbidez <= 4){
-                        barrio[i].puntos += 1;
+                        ranking[i].puntos += 1;
                     }else {
-                        barrio[i].puntos += 0;}
+                        ranking[i].puntos += 0;}
                     if(barrio[i].coliformes == 0){
-                        barrio[i].puntos += 3;
+                        ranking[i].puntos += 3;
                     }else {
-                        barrio[i].puntos += 0;}
+                        ranking[i].puntos += 0;}
                         }
-
-                   /* max =puntos;
+                    mejorfuente = ranking[0];
                        for(i=1; i< 26; i++){
-                        if (max < barrio[i].puntos){
-                            max = barrio[i].fuente;
-                            barrio[i].fuente = i;
+                        if (mejorfuente.puntos < ranking[i].puntos){
+                            mejorfuente = ranking[i].fuente;
+                            ranking[i].fuente = i;
                 }
-    }*/
-           // printf("El maximo de puntos es %s, de la fuente %s", max, barrio[i].fuente);
+    }
+            printf("El maximo de puntos es %s, de la fuente %s", mejorfuente.fuente, mejorfuente.ranking);
 
             	printf("Media conductividad: %f\n", mediac (barrio, 25)/25);
             	printf("Media conductividad: %f\n", mediaspH (barrio, 25)/25);
@@ -151,13 +156,15 @@ break;
             }}while ( opciondato != 'B' && opciondato != 'E');
 break;
 		case 2:
-			printf("bien 2");
+			printf("");
+
+
 			break;
 
 		case 3:
 			printf("bien 3");
 			break;
-		
+
 
 
 
@@ -165,7 +172,7 @@ break;
     fclose(fentrada);
 	if(opcion == 4){
 		return 0;}
-}//corchetes del int main 
+}//corchetes del int main
 void buscarph(struct Tfuente barrio[], float ph){
                         int i=0;
 
@@ -191,11 +198,11 @@ void buscarph(struct Tfuente barrio[], float ph){
                         for(i=1;i<=25;i++){
 							  if (barrio[i].pH >=7.5 && barrio[i].pH<=8)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}}}
-                                
+
 
 void buscarcond(struct Tfuente barrio[],int conductividad){
 	int i=0;
-                        
+
                         if(conductividad>=100 && conductividad<=150){
                             printf("Las conductividades mas cercanas de %d que se encuentran entre 100 y 150 son:\n", conductividad);
                           for(i=1;i<=25;i++){
@@ -211,7 +218,7 @@ void buscarcond(struct Tfuente barrio[],int conductividad){
 
                          else if(conductividad>200 && conductividad<=250){
                             printf("Las conductividades mas cercanas de %d que se encuentran entre 200 y 250 son\n", conductividad);
-                          for(i=1;i<=25;i++){     
+                          for(i=1;i<=25;i++){
 							  if (barrio[i].conductividad >=200 && barrio[i].conductividad<=250)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}}
 
@@ -226,36 +233,36 @@ void buscarcond(struct Tfuente barrio[],int conductividad){
 
 void buscarturb (struct Tfuente barrio[],int turbidez){
                         int i=0;
-                        
+
                         if(turbidez == 0){
                             printf("Las fuentes de turbidez %d son:\n", turbidez);
-                          for(i=1;i<=25;i++){    
+                          for(i=1;i<=25;i++){
 							  if (barrio[i].conductividad == 0)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}}
 
                         else if(turbidez == 1){
                             printf("Las fuentes de turbidez %d son\n", turbidez);
-                            for(i=1;i<=25;i++){ 
+                            for(i=1;i<=25;i++){
 							  if (barrio[i].conductividad == 1)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}}
 
                          else if(turbidez == 2){
                             printf("Las fuentes de turbidez %d son\n", turbidez);
                          for(i=1;i<=25;i++){
-						
+
                               if (barrio[i].conductividad == 2)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}}
 
                         else if(turbidez == 3){
                             printf("Las fuentes de turbidez %d son\n", turbidez);
-                        
+
                              for(i=1;i<=25;i++){
 							  if (barrio[i].conductividad == 3)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}}
 
                         else if(turbidez == 4){
                             printf("Las fuentes de turbidez %d son\n", turbidez);
-                         
+
                               if (barrio[i].conductividad == 4)
                                 printf("%s %f %d %d %d\n", barrio[i].fuente, barrio[i].pH, barrio[i].conductividad, barrio[i].turbidez, barrio[i].coliformes);}
 
@@ -283,8 +290,8 @@ void buscarturb (struct Tfuente barrio[],int turbidez){
 
 void buscarcoliforme(struct Tfuente barrio[], int coliformes){
                         int i=0;
-                        
-                        
+
+
                         if(coliformes == 0){
                             printf("Las fuentes con una cantidad de %d de colonias coliformes \n", coliformes);
                           for(i=1;i<=25;i++){
@@ -308,7 +315,7 @@ float mediac (struct Tfuente barrio[], int dim) {
 
 for(i=1;i<=dim;i++){
 		media += barrio[i].conductividad;}
-	
+
 
 	return (media);
 }
@@ -317,7 +324,7 @@ float mediaspH (struct Tfuente barrio[], int dim) {
 	float media=0;
 for(i=1;i<=dim;i++){
 		media += barrio[i].pH;}
-	
+
 
 	return (media);
 }
@@ -327,7 +334,7 @@ float mediasturbidez(struct Tfuente barrio[], int dim) {
 
 for(i=1;i<=dim;i++){
 		media += barrio[i].turbidez;}
-	
+
 
 	return (media);
 }
@@ -335,9 +342,9 @@ float mediascoliformes(struct Tfuente barrio[], int dim) {
 	int i=0;
 	float media=0;
 	for(i=1;i<=dim;i++){
-	
+
 		media += barrio[i].coliformes;}
-	
+
 
 	return media;}
 
